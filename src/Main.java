c004d4a4fd3cc0a810239004b7bccc4839621658
Tiago José da Silva <prog.tiago@gmail.com>
2014-10-28 12:46:02 -0200

    Fazendo uma modificação com o ti.silva

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Oi, eu sou um teste");
 		System.out.println("Fiz outra modificação");
+		System.out.println("Essa modificação foi feita pelo ti.silva@hotmail.com.br");
 	}
 
 }
