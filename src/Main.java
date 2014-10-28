2297837368b595cbf1541965fc28008394b0a587
= <prog.tiago@gmail.com>
2014-10-28 13:23:50 -0200

    Uma nova modificação com ti.silva

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Oi, eu sou um teste");
 		System.out.println("Fiz outra modificação");
 		System.out.println("Essa modificação foi feita pelo ti.silva@hotmail.com.br");
+		System.out.println("Uma nova modificação com o ti.silva");
 	}
 
 }
